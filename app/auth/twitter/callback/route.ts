c738af00dc413a2b5c02f92484ee0b11e806b732
Abdulla yamin <yarr.myn@gmail.com>
2025-09-22 00:02:06 +0500

    added force dynamic

--- app/auth/twitter/callback/route.ts
+++ app/auth/twitter/callback/route.ts
@@ -2,6 +2,9 @@ import { NextRequest, NextResponse } from 'next/server'
 import { TwitterOAuth } from '@/lib/twitter-oauth'
 import { createClient } from '@/lib/supabase/server'
 
+// Force this route to be dynamic
+export const dynamic = 'force-dynamic'
+
 export async function GET(request: NextRequest) {
   try {
     const { searchParams } = new URL(request.url)
